de1fcdee56d51d4324cc477cb9b14d7412426d02
aryner <aryner@gmail.com>
2015-03-02 10:53:27 -0800

    created a basic register page and included the jquery file

--- src/java/controller/Controller.java
+++ src/java/controller/Controller.java
@@ -6,18 +6,22 @@
 
 package controller;
 
+import model.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author aryner
  */
-@WebServlet(name = "Controller", urlPatterns = {"/Controller"})
+@WebServlet(name = "Controller", urlPatterns = {
+						"/Controller","/register"
+						})
 public class Controller extends HttpServlet {
 	/**
 	 * Handles the HTTP <code>GET</code> method.
@@ -30,6 +34,24 @@ public class Controller extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
+
+		String userPath = request.getServletPath(); 
+		HttpSession session = request.getSession(); 
+		User user = (User)session.getAttribute("user");
+
+		//The user is not logged in so is redirected to the index/login page
+		if(user == null && !userPath.equals("/register")) {
+			response.sendRedirect("/Cornea_Grader/index.jsp");
+			return;
+		}
+
+		String url = "/WEB-INF/view" + userPath + ".jsp";
+
+		try {
+			request.getRequestDispatcher(url).forward(request, response);
+		} catch (IOException ex){
+			ex.printStackTrace(System.out);
+		}
 	}
 
 	/**
@@ -43,6 +65,18 @@ public class Controller extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
+
+		String userPath = request.getServletPath(); 
+		HttpSession session = request.getSession(); 
+		User user = (User)session.getAttribute("user");
+
+		String url = "/WEB-INF/view" + userPath + ".jsp";
+
+		try {
+			request.getRequestDispatcher(url).forward(request, response);
+		} catch (IOException ex){
+			ex.printStackTrace(System.out);
+		}
 	}
 
 	/**
---
+++ web/WEB-INF/view/register.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : register
+    Created on : Mar 2, 2015, 10:29:32 AM
+    Author     : aryner
+--%>
+
+<h2>Register</h2>
+
+
+<p><a href="index.jsp">Already registered?</a></p>
+
+	<%
+		if(session.getAttribute("user") != null) {
+			response.sendRedirect("/Photo_Grader/home"); 
+		}
+		if(session.getAttribute("error") != null) {
+			out.print("<p class='error'>" + session.getAttribute("error") + "</p>");
+			session.removeAttribute("error");
+		}
+	%>
+
+<form action="createUser" method="Post">
+	<p>
+		User name: <input type="text" name="userName">
+	</p>
+
+	<p>
+		Password: <input type="password" name="password">
+	</p>
+	<p>
+		Repeat password: <input type="password" name="rePassword">
+	</p>
+	<p>
+		<input type="submit" value="submit" class="btn">
+	</p>
+</form>
